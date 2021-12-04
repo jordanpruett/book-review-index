32763605d987e5b3536b49a11474c0708e65fe4e
jordan <jordanpruett@gmail.com>
2021-12-03 17:10:30 -0800

    add flairparser.py

---
+++ extract/train/flairparser.py
@@ -0,0 +1,49 @@
+from flair.data import Corpus
+from flair.datasets import ColumnCorpus
+from flair.embeddings import FlairEmbeddings
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
+
+def main():
+
+    columns = {0: 'text', 1: 'tag'}
+
+    data_folder = 'flairparser'
+
+    corpus: Corpus = ColumnCorpus(
+        data_folder, columns,
+        train_file='train.txt',
+        test_file='test.txt',
+        dev_file='valid.txt'
+    )
+
+    tag_dictionary = corpus.make_tag_dictionary('tag')
+
+    embeddings = FlairEmbeddings(os.path.join(
+        'embeddings', 'full', 'spec_newline', 'best-lm.pt'))
+
+    tagger: SequenceTagger = SequenceTagger(
+        hidden_size=256,
+        embeddings=embeddings,
+        tag_dictionary=tag_dictionary,
+        tag_type='tag',
+        use_crf=True
+    )
+
+    trainer : ModelTrainer = ModelTrainer(tagger, corpus)
+
+    trainer.train(
+        os.path.join('labeler', 'models'),
+        learning_rate=0.1,
+        mini_batch_size=32,
+        max_epochs=150
+    )
+
+if __name__=='__main__':
+
+    if not torch.cuda.is_available():
+        print("Warning: GPU not available. Defaulting to CPU.")
+    else:
+        print("Using device: ", flair.device)
+    print(torch.cuda.is_available())
+    main()
